c6e2b033d7769aed5ded08d551017db19391bc2a
= <=>
2023-11-16 16:27:11 +0100

    defaultdict -> collections.js

---
+++ js/collections.js
@@ -0,0 +1,8 @@
+// stores primitives only
+class DefaultDict {
+    constructor(defaultVal) {
+        return new Proxy({}, {
+            get: (target, name) => name in target ? target[name] : defaultVal
+        })
+    }
+}
